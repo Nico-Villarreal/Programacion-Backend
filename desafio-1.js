604f4782af61fbea2ca22d048ef3c57729223853
Nicolas Villarreal <105127312+Nico-Villarreal@users.noreply.github.com>
2023-10-08 18:55:23 -0300

    Update desafio-1.js

--- desafio-1.js
+++ desafio-1.js
@@ -3,8 +3,23 @@ class ProductManager{
         this.products = []
     }
 
+    getProducts(){
+        return this.products
+       }
 
    addProduct(title, description, price, thumbnail, code, stock){
+    
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+        console.log("Todos los campos son obligatorios");
+        return;
+    }
+ 
+    const CodeRepeat = this.products.find(product => product.code === code);
+    if (CodeRepeat) {
+        console.log("El código de producto ya existe");
+        return;
+    }
+
     const product = {
         id: this.#getMaxId()+1,
         title, 
@@ -13,12 +28,11 @@ class ProductManager{
         thumbnail,
         code,
         stock,
+        }   
+    
+        this.products.push(product) 
     }
 
-    this.products.push(product) 
-
-
-   }
 
    #getMaxId(){
     let maxId = 0;
@@ -27,17 +41,26 @@ class ProductManager{
     })
         return maxId
    };
-  
-   getProducts(){
-    return this.products
-   }
+
+   getProductById(id) {
+    const ProductId = this.products.find((product) => product.id === id);
+
+      if (ProductId) {
+       return console.log("PRODUCTO EN LA LISTA");
+      } else {
+      console.error("NOT FOUND"); 
+        return null; 
+      }
+  }
 
 };
+
 
 const ProductsFinaly = new ProductManager()
 
 ProductsFinaly.addProduct("producto prueba", "este producto es una prueba", 200, "sin imagen", "abc123", 250)
 ProductsFinaly.addProduct("producto prueba 2", "esta es otra prueba", 300, "sigue sin imagen", "abc246", 100)
 ProductsFinaly.addProduct("producto prueba 2", "esta es otra prueba", 300, "sigue sin imagen", "abc246", 100)
-ProductsFinaly.getProducts()
+ProductsFinaly.getProductById(6)
 console.log(ProductsFinaly)
+
