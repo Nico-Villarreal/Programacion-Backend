d6e888de2dd90498d7df72783a77cc430a82ab81
Nicolas Villarreal <105127312+Nico-Villarreal@users.noreply.github.com>
2024-01-11 10:36:41 -0300

    reestructura del servidor v1.4

---
+++ desafio 11 - reestructura del servidor/client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+    return <footer className="footer">
+        <p>2023 - Portafolio <a href="https://github.com/Nico-Villarreal/Programacion-Backend">aqui</a></p>
+    </footer>
+}
+
+export default Footer;
